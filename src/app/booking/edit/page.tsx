f1af0bfdfbe7d6d6adb86809887f4f3e10e07ff1
watabegg <roy_wtnb@outlook.jp>
2024-06-13 01:37:26 +0900

    suspenceを追加

--- src/app/booking/edit/page.tsx
+++ src/app/booking/edit/page.tsx
@@ -1,13 +1,17 @@
 'use client'
 
-import React from 'react'
+import React, { Suspense } from 'react'
 import BookingEdit from '@/components/BookingEdit'
 import { useSearchParams } from 'next/navigation'
 
 const Page = () => {
 	const queryParams = useSearchParams()
 	const id = queryParams.get('id') ?? ''
-	return <BookingEdit id={id}/>
+	return (
+		<Suspense>
+			<BookingEdit id={id} />
+		</Suspense>
+	)
 }
 
 export default Page
