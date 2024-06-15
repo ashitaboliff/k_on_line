886836e92c285a2b61277c0b6329a0be032c2af7
watabegg <roy_wtnb@outlook.jp>
2024-06-15 23:56:15 +0900

    シェルの追加とちょっと編集

--- src/app/api/booking/logs/route.ts
+++ src/app/api/booking/logs/route.ts
@@ -15,7 +15,7 @@ export async function GET(req: NextRequest) {
 				name: true,
 				is_deleted: true,
 			},
-			orderBy: [{ booking_date: 'asc' }, { booking_time: 'asc' }],
+			orderBy: { created_at: 'desc' },
 		})
 		const bookingsWithJST = bookings.map((booking) => ({
 			...booking,
