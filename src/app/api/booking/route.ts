2ad666e16a0b73b353f4285bb9142e4dcec20c8f
watabegg <roy_wtnb@outlook.jp>
2024-07-26 01:00:13 +0900

    認証のためのデータベース準備

--- src/app/api/booking/route.ts
+++ src/app/api/booking/route.ts
@@ -40,6 +40,7 @@ export async function GET(req: NextRequest) {
 		}))
 		return NextResponse.json({ response: bookingsWithJST }, { status: 200 })
 	} catch (error) {
+		// console.error(error)
 		return NextResponse.json(
 			{ error: 'Failed to fetch bookings' },
 			{ status: 500 },
