179ee1195b23b3617cc9449dde014a9cad143e32
watabegg <roy_wtnb@outlook.jp>
2024-07-28 19:08:45 +0900

    Google Analyticsを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/molecules/Header'
+import { GoogleAnalytics } from "@next/third-parties/google"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -33,6 +34,7 @@ export default function RootLayout({
 				<Header />
 				{children}
 			</body>
+			<GoogleAnalytics gaId={process.env.GA_ID ?? ""} />
 		</html>
 	)
 }
